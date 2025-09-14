7b301eadebd5ca99a04edb6568a6a2a78b004997
Siyabonga Mhlongo <mhlongoquincy@gmail.com>
2025-09-14 23:18:42 +0200

    commit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://mern-expense-tracker-wheh.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
